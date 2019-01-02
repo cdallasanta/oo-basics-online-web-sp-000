172a80c7f41c5c8125b0053bcabcb30e68ae8ea4
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-02 06:04:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,5 @@
 class Book
   attr_accessor :title, :author, :pages, :genre
-  def initialize(title,author,pages,genre)
-    @title = title
-    @author = author
-    @pages = pages
-    @genre = genre
-  end
 
   def turn_page
     @pages += 1
